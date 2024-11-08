cbf59338a458e7b6a631a85f294302e864460a45
Josh Thomas <jthoms1@gmail.com>
2024-11-07 19:39:52 -0800

    Upgrade to gen2

--- src/builder-registry.ts
+++ src/builder-registry.ts
@@ -1,21 +1,23 @@
-import { Builder } from "@builder.io/react";
 import IconCard from "./components/Card/IconCard";
 import ImageHero from "./components/Hero/ImageHero";
 import SplitHero from "./components/Hero/SplitHero";
 import TextHero from "./components/Hero/TextHero";
 
-Builder.registerComponent(IconCard, {
-  name: "IconCard",
-});
-
-Builder.registerComponent(ImageHero, {
-  name: "ImageHero",
-});
-
-Builder.registerComponent(SplitHero, {
-  name: "SplitHero",
-});
-
-Builder.registerComponent(TextHero, {
-  name: "TextHero",
-});
+export const CUSTOM_COMPONENTS = [
+  {
+    component: IconCard,
+    name: "IconCard",
+  },
+  {
+    component: ImageHero,
+    name: "ImageHero",
+  },
+  {
+    component: SplitHero,
+    name: "SplitHero",
+  },
+  {
+    component: TextHero,
+    name: "TextHero",
+  },
+];
